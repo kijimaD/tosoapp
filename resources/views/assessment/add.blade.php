422554679abab4087f2fc63f9186b75a30021177
kijima <norimaking777@gmail.com>
2019-05-30 09:53:13 +0900

    査定明細ビューにタイトル、コンディション、商品テーブルの内容をくっつけた。

--- resources/views/assessment/add.blade.php
+++ resources/views/assessment/add.blade.php
@@ -3,6 +3,59 @@
 @section('title','口座登録')
 
 @section('content')
+
+<script>
+    function addList(obj) {
+  // tbody要素に指定したIDを取得し、変数「tbody」に代入
+  var tbody = document.getElementById('sub_table');
+  // objの親の親のノードを取得し（つまりtr要素）、変数「tr」に代入
+  var tr = obj.parentNode.parentNode;
+  // tbodyタグ直下のノード（行）を複製し、変数「list」に代入
+  var list = tbody.childNodes[0].cloneNode(true);
+  // 複製した行の2番目のセルを指定し、変数「td」に代入
+  var td = list.childNodes[1];
+  // 複製した行の2番目のセルの内容を「A」に置き換え
+
+  //　複製したノード「list」を直後の兄弟ノードの上に挿入
+  // （「tr」の下に挿入）
+  tbody.insertBefore(list, tr.nextSibling);
+}
+function removeList(obj) {
+  // tbody要素に指定したIDを取得し、変数「tbody」に代入
+  var tbody = document.getElementById('sub_table');
+  // objの親の親のノードを取得し（つまりtr要素）、変数「tr」に代入
+  var tr = obj.parentNode.parentNode;
+  // 「tbody」の子ノード「tr」を削除
+  tbody.removeChild(tr);
+}
+function addarea(){
+  $('#addarea').append('<div class="form-group form-inline"><div class=""><input type="text" name="isbn[]" /></div><div class=""><input type="text" name="title[]" /></div><div class=""><input type="text" name="description[]" /></div><div class=""><input type="text" name="condition_id[]" /></div><div class=""><input type="text" name="market_price[]" /></div><div class=""><input type="text" name="get_price[]" /></div><div class=""><button type="button" class="btn btn-outline-primary" onclick="addarea();">＋</button></div><div class=""><button type="button" class="btn btn-outline-primary" onclick="removearea(this);">－</button></div></div>')
+}
+
+// function addarea(){
+//   $('#addarea').append('
+//   <div class="form-group form-inline"><div class="">
+//   <select name="ProductID[]">
+//   @foreach ($items as $item)
+//   <option value="{{$item->ProductID}}">{{$item->ProductID}}:{{$item->product_name}}</option>
+//   @endforeach</select></div>
+//   <div class=""><input type="text" name="comment[]" /></div>
+//   <div class=""><input type="text" name="comment[]" /></div>
+//   <div class=""><input type="text" name="comment[]" /></div>
+//   <div class=""><input type="text" name="comment[]" /></div>
+//   <div class=""><input type="text" name="comment[]" /></div>
+//   <div class=""><button type="button" class="btn btn-outline-primary" onclick="addarea();">＋</button></div>
+//   <div class=""><button type="button" class="btn btn-outline-primary" onclick="removearea(this);">－</button></div></div>
+//   ')
+// }
+
+function removearea(obj){
+  var tbody = document.getElementById('addarea');
+  var grand = obj.parentNode.parentNode;
+  tbody.removeChild(grand);
+}
+</script>
+
 <form action="/assessment/add" method="post">
     {{csrf_field()}}
 
@@ -42,6 +95,63 @@
             </select>
         </div>
     </div>
+
+    <hr>
+
+    <!--▼サブテーブル▼-->
+    <div id="addarea" class="">
+        <div class="form-group form-inline">
+
+            {{-- <div class="">
+                <label for="ProductID[]" class="col-form-label text-md-left">ISBN</label>
+                <select name="ProductID[]">
+                    @foreach ($items as $item)
+                    <option value="{{$item->ProductID}}">{{$item->ProductID}}:{{$item->product_name}}</option>
+            @endforeach
+            </select>
+        </div> --}}
+
+        <div class="">
+            <label for="isbn[]" class="col-form-label text-md-left">ISBN</label>
+            <input type="text" name="isbn[]" />
+        </div>
+
+        <div class="">
+            <label for="title_name[]" class="col-form-label text-md-left">タイトル</label>
+            <input type="text" name="title[]" />
+        </div>
+
+        <div class="">
+            <label for="description[]" class="col-form-label text-md-left">説明文</label>
+            <input type="text" name="description[]" />
+        </div>
+
+        <div class="">
+            <label for="codition_id[]" class="col-form-label text-md-left">コンディションコード</label>
+            <input type="text" name="condition_id[]" />
+        </div>
+
+        <div class="">
+            <label for="market_price[]" class="col-form-label text-md-left">市場価格</label>
+            <input type="text" name="market_price[]" />
+        </div>
+
+        <div class="">
+            <label for="get_price[]" class="col-form-label text-md-left">買取価格</label>
+            <input type="text" name="get_price[]" />
+        </div>
+
+        <div class="">
+            <label class="col-form-label text-md-left">追加</label>
+            <button type="button" class="btn btn-outline-primary" onclick="addarea();">＋</button>
+        </div>
+        <div class="">
+            <label class="col-form-label text-md-left">削除</label>
+            <button type="button" class="btn btn-outline-primary" onclick="removearea(this);">－</button>
+        </div>
+    </div>
+    </div>
+    <!--▲サブテーブル▲-->
 
     <div class="form-group">
         <button class="btn btn-primary" type="submit">送信</button>
