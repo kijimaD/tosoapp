7ba7703f405a9fcbcf4f5662c821af23ff8cf59d
kijima <norimaking777@gmail.com>
2019-06-03 20:56:10 +0900

    アドレス帳とメニューをいい感じにした。

--- resources/views/address/index.blade.php
+++ resources/views/address/index.blade.php
@@ -4,44 +4,41 @@
 
 @section('content')
 <p><a href="/address/add">新規追加</a></p>
-<div class="table-responsive">
-    <table class="table table-striped table-bordered table-sm">
-        <thead>
-            <tr>
-                <th><a href="/hello?sort=age">郵便番号</a></th>
-                <th><a href="/hello?sort=age">都道府県ID</a></th>
-                <th><a href="/hello?sort=age">市区町村</a></th>
-                <th><a href="/hello?sort=age">以降の住所</a></th>
-                <th><a href="/hello?sort=age">顧客</a></th>
-                <th><a href="/hello?sort=age">登録日</a></th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($items as $item)
-            <tr>
-                <td>{{$item->zip}}</td>
-                <td>{{$item->prefecture->prefecture_name}}</td>
-                <td>{{$item->city}}</td>
-                <td>{{$item->address}}</td>
-                <td>{{$item->user->family_name}}{{$item->user->name}}</td>
-                <td>{{$item->created_at}}</td>
-                @if (isset($item->useraddress->id))
-                <td>既定の住所</td>
-                @else
-                <form action="/address/default/add" method="post">
-                    {{ csrf_field() }}
-                    <input type="hidden" name="addressBook_id" value="{{$item->id}}" />
-                    <input type="hidden" name="user_id" value="{{$user->id}}" />
-                    <td><input type="submit" value="既定にする" /></td>
-                </form>
-                @endif
 
-                <td><a href="/address/edit?id={{$item->id}}">修正</a></td>
-                <td><a href="/address/del?id={{$item->id}}">消去</a></td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
+<div class="row">
+    @foreach ($items as $item)
+    <div class="card col-sm-3 mx-3" style="width: 18rem;">
+        <div class="card-header bg-white">
+            <h5 class="card-title">{{$item->user->family_name}}{{$item->user->name}}</h5>
+            <a href="/address/edit?id={{$item->id}}">修正</a>
+            <a href="/address/del?id={{$item->id}}">消去</a>
+        </div>
+        <div class="card-body">
+            <p class="card-text">
+                〒{{$item->zip}}<br>
+                {{$item->prefecture->prefecture_name}}<br>
+                {{$item->city}}<br>
+                {{$item->address}}<br>
+            </p>
+        </div>
+        <div class="card-footer bg-white">
+            @if (isset($item->useraddress->id))
+            既定の住所
+            @else
+            <form action="/address/default/add" method="post">
+                {{ csrf_field() }}
+                <input type="hidden" name="addressBook_id" value="{{$item->id}}" />
+                <input type="hidden" name="user_id" value="{{$user->id}}" />
+                <input type="submit" value="既定にする" class="btn btn-outline-dark" />
+
+            </form>
+            @endif
+        </div>
+    </div>
+    @endforeach
 </div>
+
+</div>
+
 
 @endsection
--- resources/views/layouts/tosoapp.blade.php
+++ resources/views/layouts/tosoapp.blade.php
@@ -26,137 +26,86 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
 
     <div class="container-fluid">
-
-        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-            <a class="navbar-brand" href="/">とそブックス</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            管理者メニュー
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            <a class="dropdown-item" href="{{ route('admin.home')}}">adminログイン</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            インフォメニュー
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            <a class="dropdown-item" href="/">トップページ</a>
-                            <a class="dropdown-item" href="/home">ホーム</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            アカウントメニュー
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            <a class="dropdown-item" href="{{route('user.mypage')}}">マイページ</a>
-                            <a class="dropdown-item" href="/address">住所管理</a>
-                            <a class="dropdown-item" href="/bank">入金管理</a>
-                            <a class="dropdown-item" href="/entry/payment">買取申込</a>
-                            <a class="dropdown-item" href="/entry">買取履歴</a>
-                            <a class="dropdown-item" href="">進行状況確認</a>
-                            <a class="dropdown-item" href="">査定承認</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            @if(Auth::check())
-                            {{\Auth::user()->family_name}}{{ \Auth::user()->name}}
-                            @else
-                            ゲスト
+        <div class="row">
+            <nav class="navbar navbar-expand-lg navbar-dark bg-primary col-12">
+                <a class="navbar-brand" href="/">とそブックス</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                管理者メニュー
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <a class="dropdown-item" href="{{ route('admin.home')}}">adminログイン</a>
+                            </div>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                インフォメニュー
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <a class="dropdown-item" href="/">トップページ</a>
+                                <a class="dropdown-item" href="/home">ホーム</a>
+                            </div>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                アカウントメニュー
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <a class="dropdown-item" href="{{route('user.mypage')}}">マイページ</a>
+                                <a class="dropdown-item" href="/address">住所管理</a>
+                                <a class="dropdown-item" href="/bank">入金管理</a>
+                                <a class="dropdown-item" href="/entry/payment">買取申込</a>
+                                <a class="dropdown-item" href="/entry">買取履歴</a>
+                                <a class="dropdown-item" href="">進行状況確認</a>
+                                <a class="dropdown-item" href="">査定承認</a>
+                            </div>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                @if(Auth::check())
+                                {{\Auth::user()->family_name}}{{ \Auth::user()->name}}
+                                @else
+                                ゲスト
+                                @endif
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                @if(Auth::check())
+                                <a class="dropdown-item" href="{{ route('logout')}}">ログアウト</a>
+                                @else
+                                <a class="dropdown-item" href="{{ route('login')}}">ログイン</a>
+                                <a class="dropdown-item" href="{{ route('register')}}">会員登録</a>
+                                <a class="dropdown-item" href="">*パスワードを忘れた</a>
+                            </div>
                             @endif
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            @if(Auth::check())
-                            <a class="dropdown-item" href="{{ route('logout')}}">ログアウト</a>
-                            @else
-                            <a class="dropdown-item" href="{{ route('login')}}">ログイン</a>
-                            <a class="dropdown-item" href="{{ route('register')}}">会員登録</a>
-                            <a class="dropdown-item" href="">*パスワードを忘れた</a>
-                        </div>
-                        @endif
-                    </li>
-                </ul>
-            </div>
-        </nav>
-        {{-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ --}}
-        {{-- <nav class="nav nav-justified nav-tabs">
-            <div class="container-fluid bg-dark">
-                <div class="navbar-header">
-                    <a href="/" class="navbar-brand">とそブックス</a>
+                        </li>
+                    </ul>
                 </div>
-                <ul class="nav nav-tabs">
-                    <li class="dropdown nav-item">
-                        <a href="" class="nav-link dropdown-toggle" data-toggle="dropdown">アカウントメニュー</a>
-                        <div class="dropdown-menu">
-                            <a class="dropdown-item" href="{{route('user.mypage')}}">マイページ</a>
-        <a class="dropdown-item" href="/address">住所管理</a>
-        <a class="dropdown-item" href="/bank">入金管理</a>
-        <a class="dropdown-item" href="/entry/payment">買取申込</a>
-        <a class="dropdown-item" href="/entry">買取履歴</a>
-        <a class="dropdown-item" href="">進行状況確認</a>
-        <a class="dropdown-item" href="">査定承認</a>
-    </div>
-    </li>
-    <li class="dropdown nav-item">
-        <a href="" class="nav-link dropdown-toggle" data-toggle="dropdown">管理者メニュー</a>
-        <div class="dropdown-menu">
-            <a class="dropdown-item" href="{{ route('admin.home')}}">adminログイン</a>
+            </nav>
+        </div>{{-- row --}}
+
+        <div class="row justify-content-center">
+            <div class="col-8 bg-white">
+                <h2 class="page-header mt-2">@yield('title')</h2>
+            </div>
         </div>
-    </li>
 
-    <li class="dropdown nav-item">
-        <a href="" class="nav-link dropdown-toggle" data-toggle="dropdown">インフォページ</a>
-        <div class="dropdown-menu">
-            <a class="dropdown-item" href="/">トップページ</a>
-            <a class="dropdown-item" href="/home">ホーム</a>
+        <div class="row justify-content-center">
+            <div class="col-8 bg-white">
+                @yield('content')
+            </div>
         </div>
-    </li>
 
-    <li class="dropdown nav-item">
-        @if (auth::check())
-        <a href="" class="nav-link dropdown-toggle text-success" data-toggle="dropdown">:{{ \auth::user()->name}}</a>
-        <div class="dropdown-menu">
-            <a class="dropdown-item" href="{{ route('logout')}}">ログアウト</a>
+        <div class="row">
+            <div class="col-12 footer mt-3 navbar-dark bg-primary">
+                <small class="text-white">copyright 2019 TSB</small>
+            </div>
         </div>
-        @else
-        <a href="" class="nav-link dropdown-toggle text-danger" data-toggle="dropdown">ゲスト</a>
-        <div class="dropdown-menu">
-            <a class="dropdown-item" href="{{ route('login')}}">ログイン</a>
-            <a class="dropdown-item" href="{{ route('register')}}">会員登録</a>
-            <a class="dropdown-item" href="">*パスワードを忘れた</a>
-        </div>
-        @endif
-    </li><!-- ログイン状況確認 --> --}}
 
-    {{-- <li class="dropdown nav-item">
-                      <a href="" class="dropdown-toggle" data-toggle="dropdown">debug</a>
-                      <div class="dropdown-menu">
-                          <a class="dropdown-item" href="/home">ホーム</a>
-                          <a class="dropdown-item" href="/member/send">メール送信テスト</a>
-                          <a class="dropdown-item" href="/member/test">bootstrapのテスト</a>
-                      </div>
-                  </li> --}}
-
-
-    {{-- </ul>
-    </div><!-- col-xs-2 --> --}}
-
-
-    <div class="col-xs-12 col-12 bg-white">
-        <h2 class="page-header mt-2">@yield('title')</h2>
-        <hr>
-        @yield('content')
-    </div>
-    </nav>
-
-    <div class="footer navbar-dark bg-primary">
-        <small class="text-white">copyright 2019 TSB</small>
-    </div>
     </div> {{-- container --}}
 </body>
 
