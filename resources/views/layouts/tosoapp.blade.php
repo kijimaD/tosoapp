a6bd87609af1552b8e6c107b0d1c712a5564f023
kijima <norimaking777@gmail.com>
2019-06-15 07:04:21 +0900

    nav、footerを再作成した。jumbotronも。サイドを揃えた。

--- resources/views/layouts/tosoapp.blade.php
+++ resources/views/layouts/tosoapp.blade.php
@@ -18,6 +18,13 @@
         .table-responsive th {
             white-space: nowrap;
         }
+
+        .page-footer a:link{
+          color:white;
+        }
+        .page-footer a:visited{
+          color:white;
+        }
     </style>
 </head>
 
@@ -25,90 +32,140 @@
     <script src="https://code.jquery.com/jquery-3.4.0.slim.js" integrity="sha256-milezx5lakrZu0OP9b2QWFy1ft/UEUK6NH1Jqz8hUhQ=" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
 
-    <div class="container-fluid">
-        <div class="row">
-            <nav class="navbar navbar-expand-lg navbar-dark bg-primary col-12">
+    <header>
+        <nav class="navbar navbar-expand-md navbar-dark bg-primary mb-4">
+            <div class="container">
                 <a class="navbar-brand" href="/">とそブックス</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#Navbar" aria-controls="Navbar" aria-expanded="false" aria-label="ナビゲーションの切替">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="vanbar-nav mr-auto">
-                    </ul>
-                    <ul class="navbar-nav">
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                インフォメニュー
-                            </a>
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                <a class="dropdown-item" href="/">トップページ</a>
-                                <a class="dropdown-item" href="/home">ホーム</a>
-                            </div>
+                <div class="collapse navbar-collapse" id="Navbar">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">ホーム <span class="sr-only">(現位置)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">リンク</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">無効</a>
                         </li>
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                アカウントメニュー
-                            </a>
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                <a class="dropdown-item" href="{{route('user.mypage')}}">マイページ</a>
-                                <a class="dropdown-item" href="/address">住所管理</a>
-                                <a class="dropdown-item" href="/bank">口座管理</a>
-                                <a class="dropdown-item" href="/entry/add">買取申込</a>
-                                <a class="dropdown-item" href="/entry">買取履歴</a>
-                                <a class="dropdown-item" href="">進行状況確認</a>
-                                <a class="dropdown-item" href="">査定承認</a>
+                            <a href="#" class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">ドロップダウン</a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{route('user.mypage')}}">リンク1</a>
+                                <a class="dropdown-item" href="#">リンク2</a>
+                                <a class="dropdown-item" href="#">リンク3</a>
                             </div>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                @if(Auth::check())
-                                {{\Auth::user()->family_name}}{{ \Auth::user()->name}}
-                                @else
-                                ゲスト
-                                @endif
-                            </a>
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                @if(Auth::check())
-                                {{-- <a class="dropdown-item" href="{{ route('logout')}}">ログアウト</a> --}}
-                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                 document.getElementById('logout-form').submit();">
-                                    {{ __('ログアウト') }}
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                    @csrf
-                                </form>
-                                @else
-                                <a class="dropdown-item" href="{{ route('login')}}">ログイン</a>
-                                <a class="dropdown-item" href="{{ route('register')}}">会員登録</a>
-                                <a class="dropdown-item" href="">*パスワードを忘れた</a>
-                            </div>
-                            @endif
                         </li>
                     </ul>
+                    <form class="form-inline my-2 my-md-0">
+                        <input class="form-control mr-sm-2" type="search" placeholder="検索..." aria-label="検索...">
+                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">検索</button>
+                    </form>
                 </div>
-            </nav>
-        </div>{{-- row --}}
-
-        <div class="row justify-content-center">
-            <div class="col-8 bg-white">
-                <h2 class="page-header mt-2">@yield('title')</h2>
             </div>
+        </nav>
+    </header>
+
+    <main role="main" class="container">
+        <h2 class="page-header mt-2">@yield('title')</h2>
+        @yield('content')
         </div>
 
-        <div class="row justify-content-center">
-            <div class="col-sm-8 bg-white">
-                @yield('content')
-            </div>
-        </div>
+    </main>
 
-        <div class="row">
-            <div class="col-12 footer mt-3 navbar-dark bg-primary">
-                <small class="text-white">copyright 2019 とそブックス</small>
-            </div>
-        </div>
+    <footer class="page-footer font-small bg-secondary text-white">
+        <div class="container">
+            <!-- Footer Links -->
+            <div class="container-fluid text-center text-md-left">
 
-    </div> {{-- container --}}
+                <!-- Grid row -->
+                <div class="row">
+
+                    <!-- Grid column -->
+                    <div class="col-md-6 mt-md-0 mt-3">
+
+                        <!-- Content -->
+                        <h5 class="text-uppercase">Footer Content</h5>
+                        <p>Here you can use rows and columns to organize your footer content.</p>
+
+                    </div>
+                    <!-- Grid column -->
+
+                    <hr class="clearfix w-100 d-md-none pb-3">
+
+                    <!-- Grid column -->
+                    <div class="col-md-3 mb-md-0 mb-3">
+
+                        <!-- Links -->
+                        <h5 class="text-uppercase">アカウント</h5>
+
+                        <ul class="list-unstyled">
+                            <li>
+                                <a class="" href="{{route('user.mypage')}}">マイページ</a>
+                            </li>
+                            <li>
+                                <a class="" href="/address">住所管理</a>
+                            </li>
+                            <li>
+                                <a class="" href="/bank">口座管理</a>
+                            </li>
+                            <li>
+                                <a class="" href="/entry/add">買取申込</a>
+                            </li>
+                            <li>
+                                <a class="" href="/entry">買取履歴</a>
+                            </li>
+                            <li>
+                                <a class="" href="">進行状況確認</a>
+                            </li>
+                            <li>
+                                <a class="" href="">査定承認</a>
+                            </li>
+                        </ul>
+
+                    </div>
+                    <!-- Grid column -->
+
+                    <!-- Grid column -->
+                    <div class="col-md-3 mb-md-0 mb-3">
+
+                        <!-- Links -->
+                        <h5 class="text-uppercase">インフォ</h5>
+
+                        <ul class="list-unstyled">
+                            <li>
+                                <a class="" href="/">トップページ</a>
+                            </li>
+                            <li>
+                                <a class="" href="/home">ホーム</a>
+                            </li>
+                            <li>
+                                <a href="#!">Link 3</a>
+                            </li>
+                            <li>
+                                <a href="#!">Link 4</a>
+                            </li>
+                        </ul>
+
+                    </div>
+                    <!-- Grid column -->
+
+                </div>
+                <!-- Grid row -->
+
+            </div>
+            <!-- Footer Links -->
+
+            <!-- Copyright -->
+            <div class="footer-copyright text-center py-3">© 2019 Copyright:
+                <a href="https://とそ.com">とそ.com</a>
+            </div>
+            <!-- Copyright -->
+        </div>
+    </footer>
+
 </body>
 
 </html>
