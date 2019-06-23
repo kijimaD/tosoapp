de63ec6ed317cc36d1de6581492f5f09e3bf4180
kijima <norimaking777@gmail.com>
2019-06-23 14:49:33 +0900

    detailをリファクタリングした。

--- app/Http/Controllers/AssessmentdetailController.php
+++ app/Http/Controllers/AssessmentdetailController.php
@@ -7,8 +7,6 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use App\Assessment;
 use App\Assessmentdetail;
-use App\Goods;
-use App\Title;
 use App\Condition;
 
 class AssessmentdetailController extends Controller
@@ -30,84 +28,19 @@ class AssessmentdetailController extends Controller
         $param = ['items' => $items,
                   'conditions' => $conditions,
                   'sum_get_price' => $db_operations
-                  ->sum_assessment_price_column('get_price', $assessment_id),
+                  ->sum_assessment_price_column('get_price', $request),
                   'sum_market_price' => $db_operations
-                  ->sum_assessment_price_column('market_price', $assessment_id),
+                  ->sum_assessment_price_column('market_price', $request),
                   'sum_sell_price' => $db_operations
-                  ->sum_assessment_price_column('sell_price', $assessment_id),
+                  ->sum_assessment_price_column('sell_price', $request),
       ];
         return view('assessmentdetail.edit', $param);
     }
 
     public function update(Request $request)
     {
-        $goods_id = session()->pull('goods_id');
-        $title_id = session()->pull('title_id');
-        $search_amazon = new \App\lib\Amazonfunctions;
-        foreach (array_map(
-            null,
-            $goods_id,
-            $title_id,
-            $request->isbn,
-            $request->title_name,
-            $request->description,
-            $request->condition_id,
-            $request->market_price,
-            $request->get_price,
-            $request->sell_price
-            )
-            as
-          [
-            $val_goods_id,
-            $val_title_id,
-            $val_isbn,
-            $val_title_name,
-            $val_description,
-            $val_condition_id,
-            $val_market_price,
-            $val_get_price,
-            $val_sell_price
-          ]) {
-            Goods::find($val_goods_id)->update(
-                [
-            'description' => $val_description,
-            'condition_id' => $val_condition_id,
-            'market_price' => $val_market_price,
-            'get_price' => $val_get_price,
-            'sell_price' => $val_sell_price
-          ]
-        );
-            Title::find($val_title_id)->update(
-                [
-              'title_name' => $val_title_name,
-              'isbn' => $val_isbn,
-            ]
-          );
-            // 査定価格取得にチェックがあったら実行する
-            if (isset($request->flag_get_price)) {
-                $condition = Condition::where('id', $val_condition_id)->first();
-                $condition_percent = $condition->condition_percent;
-                $get_price = $val_market_price * $condition_percent;
-                Goods::find($val_goods_id)->update(
-                    [
-                    'get_price' => $get_price,
-                    'sell_price' => $val_market_price,
-                  ]
-                );
-            }
-            // タイトル取得にチェックがあったら実行する
-            if (isset($request->flag_get_title)) {
-                $goodsinfo_amazon = $search_amazon -> searchIsbn($val_isbn);
-                sleep(1);
-
-                Title::find($val_title_id)->update(
-                    [
-                  'title_name' => $goodsinfo_amazon['titlename'],
-                  'updated_at' => now(),
-                ]
-              );
-            }
-        }
+        $db_operations = new \App\lib\DbOperations;
+        $db_operations->assessmentdetails_update($request);
 
         return redirect('/assessment/admin_index');
     }
--- app/lib/DbOperations.php
+++ app/lib/DbOperations.php
@@ -2,16 +2,112 @@
 namespace App\lib;
 
 use Illuminate\Support\Facades\DB;
+use App\Goods;
+use App\Title;
+use App\Condition;
 
 class DbOperations
 {
-    public function sum_assessment_price_column($column, $assessment_id)
     // assessmentdetailsのカラムの合計額を計算する
+    public function sum_assessment_price_column($column, $request)
     {
-        $goal = DB::table('assessmentdetails')
+        $assessment_id = \Crypt::decrypt($request->id);
+        $sum = DB::table('assessmentdetails')
           ->where('assessment_id', $assessment_id)
           ->join('goods', 'goods.id', '=', 'assessmentdetails.goods_id')
           ->sum($column);
-        return $goal;
+        return $sum;
+    }
+
+    // assessmentdetailsの各データをupdateする
+    public function assessmentdetails_update($request)
+    {
+        $goods_id = session()->pull('goods_id');
+        $title_id = session()->pull('title_id');
+
+        foreach (array_map(
+            null,
+            $goods_id,
+            $title_id,
+            $request->isbn,
+            $request->title_name,
+            $request->description,
+            $request->condition_id,
+            $request->market_price,
+            $request->get_price,
+            $request->sell_price,
+            )
+            as
+          [
+            $val_goods_id,
+            $val_title_id,
+            $val_isbn,
+            $val_title_name,
+            $val_description,
+            $val_condition_id,
+            $val_market_price,
+            $val_get_price,
+            $val_sell_price
+          ]
+        ) {
+            Goods::find($val_goods_id)->update(
+                [
+            'description' => $val_description,
+            'condition_id' => $val_condition_id,
+            'market_price' => $val_market_price,
+            'get_price' => $val_get_price,
+            'sell_price' => $val_sell_price
+          ]
+        );
+            Title::find($val_title_id)->update(
+                [
+              'title_name' => $val_title_name,
+              'isbn' => $val_isbn,
+            ]
+          );
+            // 査定価格取得にチェックがあったら実行する
+            if (isset($request->flag_get_price)) {
+                $this->calc_get_price($val_condition_id, $val_market_price, $val_goods_id);
+            }
+            // タイトル取得にチェックがあったら実行する
+            if (isset($request->flag_get_title)) {
+                $this->obtain_info_by_amazon($val_isbn, $val_title_id, $val_goods_id);
+            }
+        }
+    }
+
+    // 査定価格を計算・保存する
+    public function calc_get_price($val_condition_id, $val_market_price, $val_goods_id) // メソッド内の変数を再利用したいが、やり方がわからない。
+    {
+        $condition = Condition::where('id', $val_condition_id)->first();
+        $condition_percent = $condition->condition_percent;
+        $get_price = $val_market_price * $condition_percent;
+        Goods::find($val_goods_id)->update(
+            [
+                    'get_price' => $get_price,
+                    'sell_price' => $val_market_price,
+                  ]
+                );
+    }
+
+    // タイトルを取得・保存する
+    public function obtain_info_by_amazon($val_isbn, $val_title_id, $val_goods_id)
+    {
+        $search_amazon = new \App\lib\Amazonfunctions;
+        $goodsinfo_amazon = $search_amazon -> searchIsbn($val_isbn);
+        sleep(1);
+
+        Title::find($val_title_id)->update(
+            [
+                  'title_name' => $goodsinfo_amazon['titlename'],
+                  'updated_at' => now(),
+                ]
+              );
+        Goods::find($val_goods_id)->update(
+            [
+            'market_price' => $goodsinfo_amazon['usedprice'],
+            'updated_at' => now(),
+          ]
+        );
     }
 }
