083d0bade3b3b2feb761d1df68ccea7f08b04d1b
kijima <norimaking777@gmail.com>
2019-10-12 06:32:47 +0900

    顔写真のサイズを小さくした。

--- resources/views/layouts/tosoapp.blade.php
+++ resources/views/layouts/tosoapp.blade.php
@@ -6,7 +6,6 @@
 
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=divice-width, initial-scale=1, shrink-to-fit=no" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
     <link rel="stylesheet" href="{{asset('css/custom.css')}}">
     <title>@yield('title')</title>
