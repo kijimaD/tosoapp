83d27c37121de4b02e7620a754f9f9235ab03dc9
kijima <norimaking777@gmail.com>
2019-10-12 09:01:00 +0900

    ローカルにjqueryを追加した。（作動していなかった）

--- resources/views/layouts/tosoapp.blade.php
+++ resources/views/layouts/tosoapp.blade.php
@@ -201,7 +201,8 @@
 
     </footer>
     {{-- jquery読み込み --}}
-    <script src="{{asset('js/bootstrap.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('js/jquery-3.4.1.js')}}"></script>
+    <script type="text/javascript" src="{{asset('js/bootstrap.bundle.js')}}"></script>
 </body>
 
 </html>
